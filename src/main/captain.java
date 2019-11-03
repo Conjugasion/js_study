2e45877630399b8fc3f3817de771bc830e5ec03d
pomhunter <tdf516020608@163.com>
2019-11-03 16:53:04 +0800

    Mac commit

---
+++ src/main/captain.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+/**
+ * @author Lucas
+ * @date 2019-11-03 14:52
+ */
+public class captain {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String[] s = sc.nextLine().split(" ");
+        int[] num = new int[s.length];
+        for (int i = 0; i < s.length; i++) {
+            num[i] = Integer.valueOf(s[i]);
+        }
+        for (int i = 1; i < num.length; i++) {
+            for (int j = i-1; j >= 0; j--) {
+                if (num[j+1]>num[j]) break;
+                int temp = num[j+1];
+                num[j+1] = num[j];
+                num[j] = temp;
+            }
+        }
+        System.out.println(Arrays.toString(num));
+    }
+}
